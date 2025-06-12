916abe0df3a19cbb53d312ed314965167976908b
Ram Sharvesh <rkramsharvesh@gmail.com>
2025-06-12 22:45:11 +0530

    Initial commit – clean Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>DCF Valuation App</h1>
+      <p>This is your starting point. We'll add inputs and valuation logic next.</p>
+    </div>
+  );
+}
+
+export default App;
+
